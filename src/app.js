e14d9766f5acaeac201fcf09927f26c80553c5ae
ryanbliss <smile@ryanbliss.me>
2019-07-14 19:21:50 -0600

    finished it

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { convertHtmlToDelta, convertDeltaToHtml, convertTextToDelta, } = require('node-quill-converter');
+
+app.post('/delta', (req, res) => {
+    const html = req.body.html;
+    res.send(convertHtmlToDelta(html));
+});
+
+app.post('/html', (req, res) => {
+    const delta = req.body;
+    res.send(convertDeltaToHtml(delta));
+});
+
+app.post('/text', (req, res) => {
+    const delta = req.body.delta;
+    res.send(convertTextToDelta(delta));
+});
+
+app.listen(3000, function() {
+    console.log('Quill service listening on port 3000!');
+});
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM('');
-global.window = dom.window;
-global.document = dom.window.document;
-global.Node = dom.window.Node;
